ea4646273fc150f58439c3ee4e7d63f5f6709c29
Tiala Young <tialayoung@gmail.com>
2023-06-02 13:04:05 -0600

    Comment out dev connection string to show prod error

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,43 @@
 // Update with your config settings.
 
-// /**
-//  * @type { Object.<string, import("knex").Knex.Config> }
-//  */
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
-
-  // development: {
-  //   client: 'pg',
-  //   connection: {
-  //     port: 5432,
-  //     database: 'pet_furriendly_db',
-  //     user:     'postgres',
-  //     password: 'Dinglebop22'
-  //   }
-  // },
 
   development: {
     client: 'pg',
     connection: {
-    // connectionString: process.env.DATABASE_URL,
-    connectionString: 'postgresql://postgres1:6VgF2qZGkAhiKpRmWC7nttNhKHpOPdb7@dpg-chslik2k728ud3k0vnig-a:5432/pet_furriendly_db',
-      ssl: {
-        rejectUnauthorized: false
-      },
-    },
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
-    // migrations: {
-    //   tableName: 'knex_migrations'
-    // }
-    migrations: {
-      directory: './migrations'
-    },
-    // seeds: {
-    //   directory: './seeds'
-    // },
+      port: 5432,
+      database: 'pet_furriendly_db',
+      user:     'postgres',
+      password: 'Dinglebop22'
+    }
   },
+
+  // development: {
+  //   client: 'pg',
+  //   connection: {
+  //   // connectionString: process.env.DATABASE_URL,
+  //   connectionString: 'postgresql://postgres1:6VgF2qZGkAhiKpRmWC7nttNhKHpOPdb7@dpg-chslik2k728ud3k0vnig-a:5432/pet_furriendly_db',
+  //     ssl: {
+  //       rejectUnauthorized: false
+  //     },
+  //   },
+  //   // pool: {
+  //   //   min: 2,
+  //   //   max: 10
+  //   // },
+  //   // migrations: {
+  //   //   tableName: 'knex_migrations'
+  //   // }
+  //   migrations: {
+  //     directory: './migrations'
+  //   },
+  //   // seeds: {
+  //   //   directory: './seeds'
+  //   // },
+  // },
 
   production: {
     client: 'pg',
