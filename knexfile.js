78615389bd8e4498aa0bf74106601e22b28d1db4
Tiala Young <tialayoung@gmail.com>
2023-06-02 15:17:52 -0600

    Revert back to dev env

--- knexfile.js
+++ knexfile.js
@@ -11,32 +11,31 @@ module.exports = {
   //     port: 5432,
   //     database: 'pet_furriendly_db',
   //     user:     'postgres',
-  //     password: '~~~my password~~~' // UPDATE AS NEEDED
+  //     password: '~~~local password~~~' // UPDATE AS NEEDED
   //   }
   // },
 
   // // DEV WITH RENDER's CONNECTION STRING - FOR TESTING
-  // development: {
-  //   client: 'pg',
-  //   connection: {
-  //     connectionString: 'postgres://postgres1:6VgF2qZGkAhiKpRmWC7nttNhKHpOPdb7@dpg-chslik2k728ud3k0vnig-a.oregon-postgres.render.com/pet_furriendly_db',
-  //     ssl: {
-  //       rejectUnauthorized: false
-  //     },
-  //   },
-  //   // pool: {
-  //   //   min: 2,
-  //   //   max: 10
-  //   // },
-  //   migrations: {
-  //     directory: './migrations'
-  //   },
-  // },
+  development: {
+    client: 'pg',
+    connection: {
+      connectionString: 'postgres://postgres1:6VgF2qZGkAhiKpRmWC7nttNhKHpOPdb7@dpg-chslik2k728ud3k0vnig-a.oregon-postgres.render.com/pet_furriendly_db',
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+    // pool: {
+    //   min: 2,
+    //   max: 10
+    // },
+    migrations: {
+      directory: './migrations'
+    },
+  },
 
   // PROD WITH RENDER's CONNECTION STRING
   production: {
-    // client: 'pg',
-    client: process.env.CLIENT,
+    client: 'pg',
     connection: {
       connectionString: 'postgres://postgres1:6VgF2qZGkAhiKpRmWC7nttNhKHpOPdb7@dpg-chslik2k728ud3k0vnig-a.oregon-postgres.render.com/pet_furriendly_db',
       ssl: {
@@ -53,11 +52,6 @@ module.exports = {
   }
 };
 
-// Internal DB URL:
-// postgres://postgres1:6VgF2qZGkAhiKpRmWC7nttNhKHpOPdb7@dpg-chslik2k728ud3k0vnig-a/pet_furriendly_db
-
-// External DB URL:
-// postgres://postgres1:6VgF2qZGkAhiKpRmWC7nttNhKHpOPdb7@dpg-chslik2k728ud3k0vnig-a.oregon-postgres.render.com/pet_furriendly_db
 
   // staging: {
   //   client: 'postgresql',
